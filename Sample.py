2d2e4ab8e0be17488b0817202257f26d52d0cf15
tomatih <maatteeoh@wp.pl>
2018-11-11 16:04:00 +0100

    Better Messages

--- Sample.py
+++ Sample.py
@@ -9,8 +9,8 @@ logger = logging.getLogger("SampleLogger")
 coloredlogs.install(level='INFO', logger=logger)
 
 if __name__ == '__main__':
-    logger.debug("this is a debugging message")
-    logger.info("this is an informational message")
-    logger.warning("this is a warning message")
-    logger.error("this is an error message")
-    logger.critical("this is a critical message")
+    logger.debug("Debug Message")
+    logger.info("Info Message")
+    logger.warning("A Warning")
+    logger.error("Error Message")
+    logger.critical("Critical Error Message")
